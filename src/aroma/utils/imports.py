4bbf8980dbbf8c3a0666c39bc795f0a10c04b6dc
Thibaut Durand <durand.tibo@gmail.com>
2023-05-23 21:06:26 -0700

    Add `gdown`

--- src/aroma/utils/imports.py
+++ src/aroma/utils/imports.py
@@ -28,3 +28,31 @@ def is_polars_available() -> bool:
     https://www.pola.rs/
     """
     return find_spec("polars") is not None
+
+
+#################
+#     gdown
+##################
+
+
+def check_gdown() -> None:
+    r"""Checks if the ``gdown`` package is installed.
+
+    Raises
+    ------
+        RuntimeError if the ``gdown`` package is not installed.
+    """
+    if not is_gdown_available():
+        raise RuntimeError(
+            "`gdown` package is required but not installed. "
+            "You can install `gdown` package with the command:\n\n"
+            "pip install gdown\n"
+        )
+
+
+def is_gdown_available() -> bool:
+    r"""Indicates if the ``gdown`` package is installed or not.
+
+    https://github.com/wkentaro/gdown
+    """
+    return find_spec("gdown") is not None
